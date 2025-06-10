67e6b000e9bcbd68269e4591b8fb1e36d7062c7f
limchangin <dlackddls5@naver.com>
2025-06-10 23:43:51 +0900

    fix: 안 쓰는 import 제거

--- backend/src/main/java/project/backend/domain/chat/chatmessage/dao/ChatMessageRepository.java
+++ backend/src/main/java/project/backend/domain/chat/chatmessage/dao/ChatMessageRepository.java
@@ -3,13 +3,9 @@ package project.backend.domain.chat.chatmessage.dao;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import project.backend.domain.chat.chatmessage.entity.ChatMessage;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-
-	List<ChatMessage> findByChatRoom_IdOrderBySendAtAsc(Long roomId);
 
 	List<ChatMessage> findByIdIn(List<Long> ids);
 
