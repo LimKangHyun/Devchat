db6c5ea092ce84f1b80fb06e269049357bb8c253
limkanghyun <limkh06910691@gmail.com>
2025-08-04 19:42:19 +0900

    feat: jwt 필터 /actuator/prometheus 허용

--- backend/src/main/java/project/backend/global/security/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/project/backend/global/security/filter/JwtAuthenticationFilter.java
@@ -25,45 +25,46 @@ import project.backend.auth.token.jwt.TokenStatus;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-	private final JwtProvider jwtProvider;
+    private final JwtProvider jwtProvider;
 
-	private static final List<String> WHITE_LIST = List.of(
-		"/signup",
-		"/login",
-		"/token/refresh",
-		"/actuator/health"
-	);
+    private static final List<String> WHITE_LIST = List.of(
+        "/signup",
+        "/login",
+        "/token/refresh",
+        "/actuator/health",
+        "/actuator/prometheus"
+    );
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-		FilterChain filterChain)
-		throws ServletException, IOException {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain)
+        throws ServletException, IOException {
 
-		String requestURI = request.getRequestURI();
-		log.debug("[JWT Filter] 요청 URI: {}", requestURI);
-		if (requestURI.startsWith("/github/") || WHITE_LIST.contains(request.getRequestURI())) {
-			filterChain.doFilter(request, response); // JWT 검사 건너뜀
-			return;
-		}
+        String requestURI = request.getRequestURI();
+        log.debug("[JWT Filter] 요청 URI: {}", requestURI);
+        if (requestURI.startsWith("/github/") || WHITE_LIST.contains(request.getRequestURI())) {
+            filterChain.doFilter(request, response); // JWT 검사 건너뜀
+            return;
+        }
 
-		log.info("JWT필터 도달 = {}", request.getRequestURI());
+        log.info("JWT필터 도달 = {}", request.getRequestURI());
 
-		Cookie cookie = CookieUtils.getCookie(request,
-				"accessToken")
-			.orElseThrow(() -> new CustomJwtException(TokenErrorCode.NOT_FOUND_TOKEN));
-		String accessToken = cookie.getValue();
+        Cookie cookie = CookieUtils.getCookie(request,
+                "accessToken")
+            .orElseThrow(() -> new CustomJwtException(TokenErrorCode.NOT_FOUND_TOKEN));
+        String accessToken = cookie.getValue();
 
-		TokenStatus tokenStatus = jwtProvider.validateAccessToken(accessToken);
+        TokenStatus tokenStatus = jwtProvider.validateAccessToken(accessToken);
 
-		if (tokenStatus == TokenStatus.VALID) {
-			log.info("[JWT] 유효한 토큰");
-			Authentication authentication = jwtProvider.getAuthentication(accessToken);
-			SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (tokenStatus == TokenStatus.VALID) {
+            log.info("[JWT] 유효한 토큰");
+            Authentication authentication = jwtProvider.getAuthentication(accessToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-		} else {
-			SecurityContextHolder.clearContext();
-		}
+        } else {
+            SecurityContextHolder.clearContext();
+        }
 
-		filterChain.doFilter(request, response);
-	}
+        filterChain.doFilter(request, response);
+    }
 }
