aedb8d10881e10787a45cbf90c5a4a278865a3fc
limkanghyun <limkh06910691@gmail.com>
2026-03-04 21:40:25 +0900

    feat: ChatMessageSearchBatch 추가

--- backend/src/main/java/project/backend/domain/chat/chatmessage/dao/ChatMessageRepository.java
+++ backend/src/main/java/project/backend/domain/chat/chatmessage/dao/ChatMessageRepository.java
@@ -1,19 +1,29 @@
 package project.backend.domain.chat.chatmessage.dao;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.backend.domain.chat.chatmessage.entity.ChatMessage;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
-	List<ChatMessage> findByIdIn(List<Long> ids);
+    List<ChatMessage> findByIdIn(List<Long> ids);
 
-	// 채팅방 입장 시, cursor가 null값이므로, 초반 메시지를 가져올 메서드
-	List<ChatMessage> findByChatRoom_IdOrderByIdDesc(Long roomId, Pageable pageable);
+    // 채팅방 입장 시, cursor가 null값이므로, 초반 메시지를 가져올 메서드
+    List<ChatMessage> findByChatRoom_IdOrderByIdDesc(Long roomId, Pageable pageable);
 
-	// 커서기반 무한스크롤 메서드
-	List<ChatMessage> findByChatRoom_IdAndIdLessThanOrderByIdDesc(Long roomId, Long cursor,
-		Pageable pageable);
+    // 커서기반 무한스크롤 메서드
+    List<ChatMessage> findByChatRoom_IdAndIdLessThanOrderByIdDesc(Long roomId, Long cursor,
+        Pageable pageable);
 
+    @Query("""
+        SELECT cm FROM ChatMessage cm
+        LEFT JOIN ChatMessageSearch cms ON cm.id = cms.id
+        WHERE cm.sendAt > :from
+        AND cms.id IS NULL
+        """)
+    List<ChatMessage> findMissingSearchIndex(@Param("from") LocalDateTime from, Pageable pageable);
 }
