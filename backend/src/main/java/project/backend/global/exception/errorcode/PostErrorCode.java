e6f01a0bf3b580c107d6d6aa83bfab2b9f5f2125
kanghyun <limkh06910691@gmail.com>
2026-03-12 16:09:22 +0900

    feat: PostErrorCode 추가

--- backend/src/main/java/project/backend/global/exception/errorcode/PostErrorCode.java
+++ backend/src/main/java/project/backend/global/exception/errorcode/PostErrorCode.java
@@ -15,7 +15,9 @@ public enum PostErrorCode implements ErrorCode {
     POST_MAX_COUNT_INVALID("POE-005", "모집 인원을 현재 참여 인원보다 작게 설정할 수 없습니다.", HttpStatus.BAD_REQUEST),
     ALREADY_APPLIED("POE-006", "이미 신청한 모집글입니다.", HttpStatus.CONFLICT),
     APPLICANT_NOT_FOUND("POE-007", "신청자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    CANNOT_APPLY_OWN_POST("POE-008", "본인이 작성한 모집글에는 신청할 수 없습니다.", HttpStatus.BAD_REQUEST);
+    CHATROOM_ALREADY_HAS_POST("P0E-009", "이미 게시글이 연결된 채팅방입니다.", HttpStatus.CONFLICT),
+    CANNOT_APPLY_OWN_POST("POE-008", "본인이 작성한 모집글에는 신청할 수 없습니다.", HttpStatus.BAD_REQUEST),
+    APPLY_TOO_SOON("POE-009", "거절 후 24시간이 지나야 재신청할 수 있습니다.", HttpStatus.TOO_MANY_REQUESTS);
 
     private final String code;
     private final String message;
