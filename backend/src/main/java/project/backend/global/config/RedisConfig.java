9ded9bad81b2ce9c28893731d7b3ba5b29be2574
limkanghyun <limkh06910691@gmail.com>
2026-05-09 20:45:23 +0900

    chore: 테스트 코드가 운영 RedisConfig Value를 주입받지 않도록 설정

--- backend/src/main/java/project/backend/global/config/RedisConfig.java
+++ backend/src/main/java/project/backend/global/config/RedisConfig.java
@@ -5,6 +5,7 @@ import io.lettuce.core.SocketOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
@@ -16,6 +17,7 @@ import java.time.Duration;
 import java.util.Arrays;
 
 @Configuration
+@Profile("!test")
 public class RedisConfig {
 
     @Value("${spring.data.redis.sentinel.master}")
